6ee056229822d71dd8c95888941855974e8e72f5
Your Name <thoeurn.bunthen@institute.pse.ngo>
2025-12-02 15:10:13 +0700

    first commit

---
+++ src/main/java/group3/com/demoMymemories/DemoMymemoriesApplication.java
@@ -0,0 +1,13 @@
+package group3.com.demoMymemories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMymemoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMymemoriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/group3/com/demoMymemories/DemoMymemoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package group3.com.demoMymemories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMymemoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
